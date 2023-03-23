efbc119780b0ff5d5bf62ac1eec39317ebec1c5c
Adams Adjartey <kaadjartey@gmail.com>
2023-03-23 03:08:21 +0000

    Return a Promise using this prototype function getResponseFromAPI()

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,6 @@
 function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success) resolve({ status: 200, body: 'Success' });
+    if (success) resolve({ status: 200, body: 'photo-profile-1' });
     reject(Error('The fake API is not working currently'));
   });
 }
