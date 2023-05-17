2c4d5f3abe94a71a5e91c08ca8b48655a1922cea
Adams Adjartey <melawzy421@yahoo.com>
2023-05-17 03:04:53 +0000

    2. Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    process.stdout.write(`Your name is: ${chunk}`);
-  }
-});
-
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
-});
-=======
 console.log('Welcome to Holberton School, what is your name?');
 // await input
 process.stdin.resume();
@@ -28,4 +13,3 @@ process.stdin.on('readable', () => {
     process.exit();
   }
 });
->>>>>>> 68b649088b65bccfe993b7b0608d85fbeb71d17e
